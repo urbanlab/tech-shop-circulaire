b94c1a85e4d8758792b85f7d94a0c460853eb943
Yassin Siuoda <yassin@siouda.com>
2023-04-04 12:29:55 +0200

    feat(front): user login and logout

---
+++ front/src/lib/components/Nav.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { logout } from "$lib/utils/auth";
+    import { currentUser } from "$lib/store";
+</script>
+
+<section>
+    <nav class="flex justify-between">
+        <ul>Logo ?</ul>
+        <div class="dropdown dropdown-hover">
+            {#if $currentUser}
+
+                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                <!-- svelte-ignore a11y-label-has-associated-control -->
+                <label tabindex="0" class=" m-1">{$currentUser.structure}</label>
+                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100w-52">
+                    <li>{$currentUser.username}</li>
+                    <li><a href="/logout" on:click|preventDefault={logout}>Logout</a></li>
+                </ul>
+            {/if}
+        </div>
+    </nav>
+</section>
--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
     import "../app.css";
+
+    import { onMount } from "svelte";
+  import { watchUserChange } from "../lib/store";
+
+    onMount(() => {
+        console.log("mounted");
+        watchUserChange()
+    });
   </script>
-  
+
   <slot />
